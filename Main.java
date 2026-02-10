8c268ffac26ced286ff9665d96c79dec0556c9eb
Maxd2026 <maxwell.dolan2026@gbwl.org>
2026-02-10 15:59:51 +0000

    readfile

--- Main.java
+++ Main.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
-   public static void main(String []args) {
-      System.out.println("Hello World");
+   public static void main(String []args) throws IOException {
+      // Create file, Scanner, and String[] OBJECTS
+      // use the format: ClassName objectName = new ClassName();
+      File myFile = new File("pokemon.csv");
+      Scanner scan = new Scanner(myFile);
+      String[] pokemonLines = new String[152];
+
+      // initialize a line counter
+      int i = 0; 
+      // Loop through lines in the File using Scanner methods
+      // Loop while Scanner can still find another line in the File AND our line counter is less than 10
+      while ( scan.hasNext() && (i < 152) ) {
+         // Read in the next line from the File
+         String currentLine = scan.nextLine();
+         // Assign the line to be stored in the Array
+         pokemonLines[i] = currentLine;
+         // Print out that line from the Array
+         System.out.println( pokemonLines[i] );
+         // Increment line counter
+         i++;
+      }
+
+
+
+
    }
 }
